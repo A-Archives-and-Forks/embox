2e2674c12901a4e8a916f8690c9e406079a45f8d
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-02-11 20:02:27 +0300

    compat: Add src/compat/posix/include/defines/ucontext_t_define.h

---
+++ src/compat/posix/include/defines/mcontext_t_define.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * @date Jul 19, 2022
+ * @author Anton Bondarev
+ */
+
+#ifndef SRC_COMPAT_POSIX_INCLUDE_DEFINES_MCONTEXT_T_DEFINE_H_
+#define SRC_COMPAT_POSIX_INCLUDE_DEFINES_MCONTEXT_T_DEFINE_H_
+
+typedef int mcontext_t;
+
+#endif /* SRC_COMPAT_POSIX_INCLUDE_DEFINES_MCONTEXT_T_DEFINE_H_ */
