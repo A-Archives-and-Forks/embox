d2e47c8e6ee4e49f11dafa64b35ec74f0704ab0e
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-02-13 12:03:09 +0300

    compat: Add stub for sys/time/lutimes

---
+++ src/compat/posix/sys/time/lutimes/lutimes_stub.c
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 12.021.2026
+ * @author Anton Bondarev
+ */
+
+#include <util/log.h>
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+int lutimes(const char *path, const struct timeval times[2]) {
+	log_debug("path - %s", path);
+	return SET_ERRNO(-ENOSYS);
+}
