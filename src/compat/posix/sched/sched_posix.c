443fe69df9cba1428e37b27682dc455501aaa826
Anton Bondarev <anton.bondarev2310@gmail.com>
2026-02-20 14:29:38 +0300

    compat: Improve sched_setscheduler()

--- src/compat/posix/sched/sched_posix.c
+++ src/compat/posix/sched/sched_posix.c
@@ -4,8 +4,11 @@
  *  Created on: 5 feb. 2013
  *      Author: fsulima
  */
+#include <util/log.h>
 
+#include <errno.h>
 #include <sched.h>
+#include <pthread.h>
 
 #include <kernel/sched.h>
 
@@ -15,13 +18,36 @@ int sched_yield(void) {
 }
 
 int sched_get_priority_min(int policy) {
-	return SCHED_PRIORITY_MIN;
+	switch(policy) {
+		case SCHED_OTHER:
+			return SCHED_OTHER_PRIORITY_MIN;
+		case SCHED_FIFO:
+		case SCHED_RR:
+			return SCHED_RT_PRIORITY_MIN;
+		default:
+			return SET_ERRNO(-EINVAL);
+	}	
 }
 
 int sched_get_priority_max(int policy) {
-	return SCHED_PRIORITY_MAX;
+	switch(policy) {
+		case SCHED_OTHER:
+			return SCHED_OTHER_PRIORITY_MAX;
+		case SCHED_FIFO:
+		case SCHED_RR:
+			return SCHED_RT_PRIORITY_MAX;
+		default:
+			return SET_ERRNO(-EINVAL);
+	}
 }
 
 int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param) {
+	if (pid == 0) {
+		pthread_setschedparam(pthread_self(), policy, param);
+	} else {
+		log_debug("stub %s(%ld, %d, )", __func__, (long) pid, policy);
+	}
+
 	return 0;
 }
+
