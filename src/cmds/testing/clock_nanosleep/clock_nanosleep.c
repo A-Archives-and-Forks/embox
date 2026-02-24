0e27c6c7f6c16ed021c506bb163629cfc06b7818
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2026-02-24 18:47:15 +0300

    cmds/testing: Improve cmd for testing clock_nanosleep

--- src/cmds/testing/clock_nanosleep/clock_nanosleep.c
+++ src/cmds/testing/clock_nanosleep/clock_nanosleep.c
@@ -5,21 +5,33 @@
  * @author Anton Bondarev
  */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
+#include "kernel/time/time.h"
 
 int main(int argc, char **argv) {
-	struct timespec now, timetowake;
+	if (argc != 2) {
+		printf("Usage: clock_nanosleep USEC\n");
+		return EINVAL;
+	}
+	int usec = atoi(argv[1]);
+	struct timespec now, time_to_wake, time_to_sleep, diff;
+
+	time_to_sleep = ns_to_timespec(usec * NSEC_PER_USEC);
+
 	clock_gettime(CLOCK_MONOTONIC, &now);
-	timetowake = now;
-	timetowake.tv_sec += 1;
-	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &timetowake, NULL);
+	time_to_wake = timespec_add(now, time_to_sleep);
+	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &time_to_wake, NULL);
 	clock_gettime(CLOCK_MONOTONIC, &now);
-	printf("Must wake at %d sec %d nsec\n", timetowake.tv_sec, timetowake.tv_nsec);
+	printf("Must wake at %d sec %d nsec\n", time_to_wake.tv_sec, time_to_wake.tv_nsec);
 	printf("Actual wakeup time: %d sec %d nsec\n", now.tv_sec, now.tv_nsec);
-	printf("Diff: %d sec %d nsec\n", now.tv_sec - timetowake.tv_sec,
-	    now.tv_nsec - timetowake.tv_nsec);
+
+	diff = timespec_sub(now, time_to_wake);
+	printf("Diff: %d sec %d nsec\n",diff.tv_sec,
+	    diff.tv_nsec);
 	return 0;
 }
